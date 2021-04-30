878a21213a1590d26f57c2d452321ca14d485c9e
Timittra Islam Hridi <timittra.cse.aust@gmail.com>
2021-04-30 16:28:29 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDv6SHjJUbqcxSmkDeUxmxDus6rPku1yl0",
+  authDomain: "burj-al-arab-by-hridi.firebaseapp.com",
+  projectId: "burj-al-arab-by-hridi",
+  storageBucket: "burj-al-arab-by-hridi.appspot.com",
+  messagingSenderId: "828792400605",
+  appId: "1:828792400605:web:aca70099921e1aa3f0c934"
+};
+
+export default firebaseConfig;
